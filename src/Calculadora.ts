aae5903fe0577f25b7624f44b861bf894e1fd40a
Jose-Herculano-Neto <herculano.neto@hotmail.com>
2023-08-11 10:38:09 -0300

    Calculator

---
+++ src/Calculadora.ts
@@ -0,0 +1,14 @@
+export class Calculadora {
+    public somar(num1: number, num2: number): number {
+        return num1 + num2;
+    }
+    public subtrair(num1: number, num2: number): number {
+        return num1 - num2;
+    }
+    public multiplicar(num1: number, num2: number): number {
+        return num1 * num2;
+    }
+    public dividir(num1: number, num2: number): number {
+        return num1 / num2;
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+function Menu(){        
+    console.log("Calculadora");
+    console.log("1 - Somar");
+    console.log("2 - Subtrair");
+    console.log("3 - Multiplicar");
+    console.log("4 - Dividir");
+    console.log("5 - Limpar");
+    console.log("6 - Sair");
+}
+
+Menu();
