2a9a4c531b8c2409ddaa067101cb35fd30e63e9d
Jose-Herculano-Neto <herculano.neto@hotmail.com>
2023-08-12 09:40:32 -0300

    Calculadora

--- src/Calculadora.ts
+++ src/Calculadora.ts
@@ -1,14 +1,19 @@
 export class Calculadora {
     public somar(num1: number, num2: number): number {
-        return num1 + num2;
+      return num1 + num2;
     }
     public subtrair(num1: number, num2: number): number {
-        return num1 - num2;
+      return num1 - num2;
     }
     public multiplicar(num1: number, num2: number): number {
-        return num1 * num2;
+      return num1 * num2;
     }
     public dividir(num1: number, num2: number): number {
+      if (num2 !== 0) {
         return num1 / num2;
+      } else {
+        throw new Error("Não é divisivel por zero.");
+      }
     }
-}
+  }
+  
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,57 @@
-function Menu(){        
-    console.log("Calculadora");
-    console.log("1 - Somar");
-    console.log("2 - Subtrair");
-    console.log("3 - Multiplicar");
-    console.log("4 - Dividir");
-    console.log("5 - Limpar");
-    console.log("6 - Sair");
+import readlineSync from 'readline-sync';
+import { Calculadora } from './Calculadora';
+
+const calcular = new Calculadora();
+
+function Menu(): void {
+  console.log("Calculadora");
+  console.log("1 - Somar");
+  console.log("2 - Subtrair");
+  console.log("3 - Multiplicar");
+  console.log("4 - Dividir");
+  console.log("5 - Sair");
 }
 
-Menu();
+function getUserChoice(): number {
+  return readlineSync.questionInt("Escolha uma opção: ");
+}
+
+function operacao(operation: (a: number, b: number) => number): void {
+  const a = readlineSync.questionFloat("Digite o primeiro número: ");
+  const b = readlineSync.questionFloat("Digite o segundo número: ");
+  const result = operation(a, b);
+  console.log("Resultado:", result);
+}
+
+function usarChoice(choice: number): void {
+  switch (choice) {
+    case 1:
+      operacao(calcular.somar.bind(calcular));
+      break;
+    case 2:
+      operacao(calcular.subtrair.bind(calcular));
+      break;
+    case 3:
+      operacao(calcular.multiplicar.bind(calcular));
+      break;
+    case 4:
+      operacao(calcular.dividir.bind(calcular));
+      break;
+    case 5:
+      console.log("Saindo...");
+      break;
+    default:
+      console.log("Opção inválida. Por favor, escolha novamente.");
+      break;
+  }
+}
+
+let choice = 0;
+
+while (choice !== 5) {
+  Menu();
+  choice = getUserChoice();
+  usarChoice(choice);
+}
+
+console.log("Programa encerrado.");
